55f811a8bff73c146ea2edca8bca6f6ce7a6a6ec
Shawqi Hatem <shawqicauses@outlook.com>
2024-10-08 19:34:40 +0200

    Update: Enhance through finishing `/sign-in` page

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT - 01
 
 import {zodResolver} from "@hookform/resolvers/zod"
+import dynamic from "next/dynamic"
 import Link from "next/link"
-import {Fragment} from "react"
+import {useRouter} from "next/navigation"
 import {useForm} from "react-hook-form"
+import {toast} from "sonner"
 import {z} from "zod"
+import trpc from "../../../client"
 import {
   Button,
   Form,
@@ -17,40 +20,73 @@ import {
   FormMessage,
   Input
 } from "../../../components/ui"
+import {errors, successes} from "../../../lib/messages"
 import {signInSchema} from "../../../lib/schema"
 
+const Loading = dynamic(() => import("../../../components/loading"))
+
 const SignIn = function SignIn() {
+  const router = useRouter()
+  const {mutate, isPending: isSignInPending} = trpc.authRouter.signIn.useMutation({
+    onError(error) {
+      if (error.data?.code === "UNAUTHORIZED") {
+        toast(errors.AUTH[error.data.code].title, {
+          description: errors.AUTH[error.data.code].description
+        })
+        return
+      }
+
+      toast(errors.DEFAULT.title, {description: errors.DEFAULT.description})
+    },
+    onSuccess() {
+      toast(successes.AUTH.SIGN_IN.title, {description: successes.AUTH.SIGN_IN.description})
+      if (origin) {
+        router.push(`/${origin}`)
+        return
+      }
+
+      router.push("/")
+    }
+  })
+
   const form = useForm<z.infer<typeof signInSchema>>({
     resolver: zodResolver(signInSchema),
+    mode: "onChange",
     defaultValues: {
       email: "",
       password: ""
     }
   })
 
+  const onSubmit = async function onSubmit(data: z.infer<typeof signInSchema>) {
+    await mutate({
+      email: data.email,
+      password: data.password
+    })
+  }
+
+  if (isSignInPending) return <Loading className="text-primary" />
+
   return (
-    <Fragment>
+    <div className="mx-auto flex min-h-screen w-full max-w-xl-3 flex-col items-center justify-center px-4 py-24 sm:px-6 sm:py-32 lg:px-8">
       <div>
-        <h2 className="text-xl-2 font-bold leading-9 tracking-tight text-foreground">
+        <h2 className="text-center text-xl-2 font-bold leading-9 tracking-tight text-foreground">
           Sign in to your account<span className="ml-0.5 text-primary">.</span>
         </h2>
-        <p className="mt-2 text-sm leading-6 text-muted-foreground">
-          Do not have an account?
-          <Button variant="link" className="ml-1 p-0" asChild>
-            <Link href="/sign-up">Create one</Link>
-          </Button>
+        <p className="mt-4 max-w-xl text-center text-base leading-relaxed text-muted-foreground">
+          Please, provide us with your email and password to sign you in your account and go back to
+          planning your Umrah journey.
         </p>
       </div>
-
-      <div className="mt-10">
+      <div className="mt-10 w-full max-w-md">
         <div>
           <Form {...form}>
-            <form>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="grid grid-cols-6 gap-6">
               <FormField
                 name="email"
                 control={form.control}
                 render={({field}) => (
-                  <FormItem className="mb-4">
+                  <FormItem className="col-span-6">
                     <FormLabel>Email</FormLabel>
                     <FormControl>
                       <Input {...field} type="email" />
@@ -59,12 +95,11 @@ const SignIn = function SignIn() {
                   </FormItem>
                 )}
               />
-
               <FormField
                 name="password"
                 control={form.control}
                 render={({field}) => (
-                  <FormItem className="mb-2">
+                  <FormItem className="col-span-6">
                     <FormLabel>Password</FormLabel>
                     <FormControl>
                       <Input {...field} type="password" />
@@ -73,23 +108,23 @@ const SignIn = function SignIn() {
                   </FormItem>
                 )}
               />
-
-              <div className="mb-4 text-sm leading-6">
-                <Button variant="link" className="p-0" asChild>
-                  <Link href="/forgot-password">Forgot password?</Link>
-                </Button>
-              </div>
-
-              <div>
+              <div className="col-span-6 flex flex-col items-center justify-start gap-3">
                 <Button size="lg" type="submit" className="w-full">
                   Sign in
                 </Button>
+                <p className="text-sm text-muted-foreground">
+                  Do not have an account?{" "}
+                  <Link href="/sign-up" className="text-foreground underline">
+                    Sign up
+                  </Link>
+                  .
+                </p>
               </div>
             </form>
           </Form>
         </div>
       </div>
-    </Fragment>
+    </div>
   )
 }
 
