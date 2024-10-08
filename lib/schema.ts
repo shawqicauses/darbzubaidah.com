c6288303f965227f003b0a1a538ad059ea831f42
Shawqi Hatem <shawqicauses@outlook.com>
2024-10-08 18:40:43 +0200

    New: Enhance through updating `zod` schemas

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,16 +1,32 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT - 01
 import {z} from "zod"
 
 export const signUpSchema = z
   .object({
+    firstName: z.string().min(1).max(64),
+    lastName: z.string().min(1).max(64),
+    dateOfBirth: z.date(),
+    nationality: z.string().min(1).max(64),
+    passportNumber: z.preprocess(
+      (value) => parseInt(z.string().parse(value), 10),
+      z.number({message: "Expected number, received string"}).int().min(1)
+    ),
     email: z.string().email().min(1).max(64),
     password: z.string().min(1).max(64),
     passwordConfirmation: z.string().min(1).max(64)
+  })
+  .refine((data) => data.nationality !== "select-a-country", {
+    path: ["nationality"],
+    message: "Please, select a nationality."
   })
   .refine((data) => data.password === data.passwordConfirmation, {
     path: ["passwordConfirmation"],
     message: "Passwords do not match"
   })
+
+export const emailVerificationSchema = z.object({
+  token: z.string().min(1)
+})
 
 export const signInSchema = z.object({
   email: z.string().email().min(1).max(64),
