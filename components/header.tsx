ccb519a0de7f0827338f9f5a8f057f12581936b0
Shawqi Hatem <shawqicauses@outlook.com>
2024-10-08 19:03:07 +0200

    Update: Enhance through updating `Header`

--- components/header.tsx
+++ components/header.tsx
@@ -1,4 +1,4 @@
-// DONE REVIEWING: GITHUB COMMIT - 02
+// DONE REVIEWING: GITHUB COMMIT - 03
 import Image from "next/image"
 import Link from "next/link"
 import {Button} from "./ui"
@@ -12,11 +12,11 @@ const Header = function Header() {
             <div className="mx-auto max-w-xl-2 lg:mx-0">
               <div className="hidden sm:mb-10 sm:flex">
                 <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-foreground ring-1 ring-foreground/10 hover:ring-foreground/20">
-                  Discover our new pre-arranged Umrah option.{" "}
+                  Start planning your Umrah journey.{" "}
                   <Link
-                    href="/pre-arranged-umrah"
+                    href="#umrah-options"
                     className="whitespace-nowrap font-medium text-primary">
-                    Read more <span aria-hidden="true">&rarr;</span>
+                    Existing Packages <span aria-hidden="true">&rarr;</span>
                   </Link>
                 </div>
               </div>
@@ -26,12 +26,12 @@ const Header = function Header() {
               </h1>
               <p className="mt-6 text-lg leading-8 text-muted-foreground">
                 Dar Abo Zubaidah is the first application that can take away the confusion that
-                happens to you while planning your Umrah. We have a pre-arranged Umrah option and a
-                fully-customized one.
+                happens to you while planning your Umrah journey. We have existing packages and a
+                fully-customized ones.
               </p>
               <div className="mt-10 flex items-center gap-x-4">
                 <Button size="lg" asChild>
-                  <Link href="/pre-arranged-umrah">Get started</Link>
+                  <Link href="/packages-existing">Get started</Link>
                 </Button>
                 <Button variant="outline" size="lg" className="gap-3" asChild>
                   <Link href="/about">
@@ -49,7 +49,7 @@ const Header = function Header() {
           viewBox="0 0 100 100"
           preserveAspectRatio="none"
           aria-hidden="true"
-          className="absolute inset-y-0 left-0 z-20 h-full w-80 -translate-x-1/2 transform fill-background">
+          className="absolute inset-y-0 left-0 z-20 hidden h-full w-80 -translate-x-1/2 transform fill-background lg:block">
           <polygon points="0,0 90,0 50,100 0,100" />
         </svg>
         <div className="absolute inset-0 z-10 bg-primary mix-blend-multiply" />
@@ -57,7 +57,7 @@ const Header = function Header() {
           src="/assets/header-image.jpg"
           alt="Umrah"
           fill
-          className="!static aspect-[3/2] !h-auto !w-auto object-cover grayscale lg:aspect-auto lg:!h-full lg:!w-full"
+          className="!static aspect-[3/2] !h-auto object-cover grayscale lg:aspect-auto lg:!h-full lg:!w-full"
         />
       </div>
     </header>
