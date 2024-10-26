16f9a14012dbd62c85bfbe07f15654e06aec6eaa
Shawqi Hatem <shawqicauses@outlook.com>
2024-10-26 03:05:35 +0200

    New: Enhance through updating `access` field in `Airlines` collection

--- server/collections/airlines.ts
+++ server/collections/airlines.ts
@@ -1,9 +1,12 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT - 01
 import {CollectionConfig} from "payload/types"
+import {isAdmin, isAnyone} from "../../lib/payload/access"
+import {isHidden} from "../../lib/payload/admin"
 
 const Airlines: CollectionConfig = {
   slug: "airlines",
-  admin: {useAsTitle: "name"},
+  admin: {hidden: isHidden, useAsTitle: "name"},
+  access: {read: isAnyone, create: isAdmin, update: isAdmin, delete: isAdmin},
   labels: {singular: "Airline", plural: "Airlines"},
   fields: [
     {
